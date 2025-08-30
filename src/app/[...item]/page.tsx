642170c9dcbe34aa7f0b4499cad041153be23c0d
김예안 <kjan0929@naver.com>
2025-08-30 21:07:58 +0900

    setting: 라우팅 세팅

--- src/app/[...item]/page.tsx
+++ src/app/[...item]/page.tsx
@@ -4,7 +4,6 @@ import { ROUTE_PATH, isParentRoute, routePaths, routes } from "@/routes";
 
 const ItemPage = ({ params: { item } }: { params: { item: string[] } }) => {
   const path = ["", ...item].join("/") as ROUTE_PATH;
-  console.log(path);
 
   const route = routes[path];
   if (!routePaths.includes(path) || isParentRoute(route) || !route.children)
